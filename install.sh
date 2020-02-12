12dfe0aba77c61287c21f56247b9370fed24f53b
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-02-12 01:14:09 -0800

    setup allows for reainstall

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ cd $FOLDER
 # yes | sudo pip3 install pygame
 
 yes | sudo pip3 install ds4drv
-sudo python3 setup.py install
+sudo python3 setup.py clean --all install
 
 for file in *.service; do
 	[ -f "$file" ] || break
